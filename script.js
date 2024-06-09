fa1c45b95cc495bb226cd0728a5c7f4b21126a9c
unknown <anil.com>
2024-06-09 10:01:15 +0545

    changed upload button styling

---
+++ script.js
@@ -0,0 +1,23 @@
+const preview = document.querySelector(".photo-preview");
+const photoInput = document.querySelector("#photoInput");
+const slicesImg = document.querySelectorAll(".slice img");
+
+photoInput.addEventListener("change", () => {
+  const file = photoInput.files[0];
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    const img = document.createElement("img");
+    img.src = reader.result;
+    img.classList.add("image");
+    preview.innerHTML = "";
+    preview.appendChild(img);
+
+    //add src to each img node
+    slicesImg.forEach((element) => {
+      element.src = reader.result;
+    });
+  };
+
+  reader.readAsDataURL(file);
+});
