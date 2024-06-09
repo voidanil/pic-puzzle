169aae0555370ea4fb6ade5faf3447f5bfa91733
unknown <anil.com>
2024-06-09 16:06:12 +0545

    applied translate property to crop

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const preview = document.querySelector(".photo-preview");
 const photoInput = document.querySelector("#photoInput");
+const slices = document.querySelectorAll(".slice");
 const slicesImg = document.querySelectorAll(".slice img");
 
+const drag = (ev) => {
+  ev.dataTransfer.setData("text", ev.target.id);
+};
+
+const allowDrop = (ev) => {
+  ev.preventDefault();
+};
+
+const drop = (ev) => {
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  const dataElement = document.getElementById(data);
+  //   ev.target.appendChild(dataElement);
+};
+
+slices.forEach((element) => {
+  element.ondrop = drop;
+  element.ondragover = allowDrop;
+});
+
+// handling the image image upoad
 photoInput.addEventListener("change", () => {
   const file = photoInput.files[0];
   const reader = new FileReader();
@@ -16,6 +38,8 @@ photoInput.addEventListener("change", () => {
     //add src to each img node
     slicesImg.forEach((element) => {
       element.src = reader.result;
+      element.draggable = true;
+      element.ondragstart = drag;
     });
   };
 
